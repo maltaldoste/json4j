9d14d5156a993b783a5b507ebf0a00d978a6e110
Malte Dostal <malte.dostal@gmail.com>
2025-07-02 12:41:33 +0200

    JsonArray: add @ToString and make value @Delegate

--- lib/src/main/java/de/malte/json4j/JsonArray.java
+++ lib/src/main/java/de/malte/json4j/JsonArray.java
@@ -3,6 +3,8 @@ package de.malte.json4j;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.Delegate;
 
 import java.util.List;
 
@@ -11,10 +13,11 @@ import java.util.List;
  */
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
+@ToString
 public class JsonArray extends JsonElement {
     /**
      * The array of elements.
      */
     @Getter
-    private List<JsonElement> array;
+    private @Delegate List<JsonElement> array;
 }
